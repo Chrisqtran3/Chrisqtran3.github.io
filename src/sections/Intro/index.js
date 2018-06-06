e63009f9e58de2c3d10b44699a8f257ccc20e98b
Chris <tran.chris10@gmail.com>
2018-06-05 23:48:30 -0500

    Use smaller image

--- src/sections/Intro/index.js
+++ src/sections/Intro/index.js
@@ -9,7 +9,7 @@ const Intro = () => {
     <section className='intro-section'>
       <div className='container'>
         <Jumbotron className='jumbotron'>
-          <img src='//placehold.it/250x250' alt='Intro' className='intro-img' />
+          <img src='//placehold.it/200x200' alt='Intro' className='intro-img' />
           <h1>Web Developer</h1>
           <hr />
           <p>HTML/CSS | Bootstrap | JavaScript | React | NodeJS | Express | MongoDB</p>
